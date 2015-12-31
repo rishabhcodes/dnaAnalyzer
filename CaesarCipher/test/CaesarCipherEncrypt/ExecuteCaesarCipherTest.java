0dd89b478eb9f2a83cddfecdde0cae9348fb3c59
risshah <rishabhcodes@gmail.com>
2015-12-31 11:29:37 +0530

    Currently an empty junit. Will be populated in a bit

--- CaesarCipher/test/CaesarCipherEncrypt/ExecuteCaesarCipherTest.java
+++ CaesarCipher/test/CaesarCipherEncrypt/ExecuteCaesarCipherTest.java
@@ -7,18 +7,15 @@ import org.junit.Before;
 import org.junit.Test;
 
 public class ExecuteCaesarCipherTest {
-    private ExecuteCaesarCipher ce;
-
+    
 	@Before
 	public void setUp() throws Exception {
-		ce = new ExecuteCaesarCipher("me the tumee keep your tumee");
+		
 	}
-
-
 	
 	@Test
 	public void test_simple_test(){
-		assertArrayEquals(new String[] {"Me the tumee, keep your tumee", "Ew lzw lmeww, cwwh qgmj lmeww", "Me the tumee, keep your tumee"}, ExecuteCaesarCipher.simpleTests());
+		
 	}
 	
 
